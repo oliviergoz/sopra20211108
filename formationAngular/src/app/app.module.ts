741613743b896ab37c118ce11fd73d8b4136449b
Unknown <o.gozlan@ajc-ingenierie.fr>
2021-12-03 10:52:04 +0100

    exo input

--- formationAngular/src/app/app.module.ts
+++ formationAngular/src/app/app.module.ts
@@ -5,9 +5,12 @@ import { FirstComponent } from './first/first.component';
 import { FormsModule } from '@angular/forms';
 import { CouleurComponent } from './couleur/couleur.component';
 import { ProduitComponent } from './produit/produit.component';
+import { TestSelectorComponent } from './test-selector/test-selector.component';
+import { AscBoldElementComponent } from './asc-bold-element/asc-bold-element.component';
+import { AscTooltipComponent } from './asc-tooltip/asc-tooltip.component';
 
 @NgModule({
-  declarations: [AppComponent, FirstComponent, CouleurComponent, ProduitComponent],
+  declarations: [AppComponent, FirstComponent, CouleurComponent, ProduitComponent, TestSelectorComponent, AscBoldElementComponent, AscTooltipComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
